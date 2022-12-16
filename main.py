81d9f5931de3822afeacb0852bf06fa691a43225
tijsziere <39266480+tijsziere@users.noreply.github.com>
2022-12-16 15:59:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Header
 from pydantic import BaseModel
 
 import requests
@@ -11,10 +11,17 @@ import click
 import shutil
 load_dotenv(dotenv_path=".env")
 
-class Kobo(BaseModel):
-    kobotoken: str
+class KoboDrive(BaseModel):
     gdriveurl: str
-    koboid: int
+    id: int
+    class Config:
+        fields = {'id':'_id'}
+
+class Kobo(BaseModel):
+    id: int
+    class Config:
+        fields = {'id':'_id'}
+
 
 app = FastAPI()
 
@@ -35,15 +42,25 @@ async def create_upload_file(mappingcsv: UploadFile):
         shutil.copyfileobj(mappingcsv.file, buffer)
 
     return {"Result": "OK", "filename": mappingcsv.filename}
-
-@app.post("/kobo/{assetid}")
-async def kobo(assetid: str, kobo:Kobo):
+    print(kobo)
     kobo = dict(kobo)
-    token = kobo['kobotoken']
+    print(kobo)
+    koboid = kobo['id']
+    print(koboid)
+    return {
+        "HeaderTest": kobotoken,
+        "MappingFile": mappingfile,
+        "koboId": koboid,
+        "assetid": assetid
+        }
+
+@app.post("/kobo/gdrive/{assetid}")
+async def kobo(assetid: str, kobo:KoboDrive, kobotoken: str = Header(default=None)):
+    kobo = dict(kobo)
     url = kobo['gdriveurl']
-    koboid = kobo['koboid']
+    koboid = kobo['id']
     # Get latest KoBo Submission
-    df = get_kobo_data_id(assetid,koboid,token)
+    df = get_kobo_data_id(assetid,koboid,kobotoken)
     # remove group names
     for key in list(df.keys()):
         new_key = key.split('/')[-1]
@@ -52,6 +69,41 @@ async def kobo(assetid: str, kobo:Kobo):
     # Create a dataframe to map the Kobo question names to the Espo Fieldnames, mapping csv should be publicly on google drive
     url='https://drive.google.com/uc?id=' + url.split('/')[-2]
     mapping = pd.read_csv(url, header=0, index_col=0, squeeze=True)
+
+    # Create API payload body
+    payload = {}
+    
+    for ix, row in mapping.iterrows():
+        field = row['esponame']  # field in espo
+        question = row['koboname']  # question in kobo
+        question_type = row['type']  # question type in kobo
+        try:
+            # If select_multiple questions, split up string, turn into list (array in json)
+            if question_type == 'select_multiple':
+                payload_value = df[question].split()
+            # If no conditions apply, map right value
+            else:
+                payload_value = df[question]
+        # If field is not filled in KoBo survey, pass empty string
+        except KeyError:
+            payload_value = ''
+        payload[field] = payload_value
+
+    return payload
+
+@app.post("/kobo/{assetid}")
+async def kobo(assetid: str, kobo:Kobo, kobotoken: str = Header(default=None), mappingfile: str = Header(default=None)):
+    kobo = dict(kobo)
+    koboid = kobo['id']
+    # Get latest KoBo Submission
+    df = get_kobo_data_id(assetid,koboid,kobotoken)
+    # remove group names
+    for key in list(df.keys()):
+        new_key = key.split('/')[-1]
+        df[new_key] = df.pop(key)
+    
+    # Create a dataframe to map the Kobo question names to the Espo Fieldnames, mapping csv should be publicly on google drive
+    mapping = pd.read_csv(mappingfile, header=0, index_col=0, squeeze=True)
 
     # Create API payload body
     payload = {}
